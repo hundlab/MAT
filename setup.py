2e144ea33b7401ea35aaca56461410903f695701
Alex <alexjwinkle@hotmail.com>
2019-09-03 16:01:29 -0400

    Added Setup

---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os
+from setuptools import setup
+
+install_requires = [
+'matplotlib',
+'scipy',
+'scikit-image',
+'sklearn',
+'xlrd',
+'xlsxwriter',
+'csv',
+'numpy',
+'PyQt5',
+'PIL']
+
+setup(name='Macrophage Analysis Toolkit',
+      version='1.0',
+      description='A tool to quantify macrophage presence',
+      author='Alexander Winkle',
+      author_email='Hund.1@osu.edu',
+      install_requires=install_requires
+      )
+
